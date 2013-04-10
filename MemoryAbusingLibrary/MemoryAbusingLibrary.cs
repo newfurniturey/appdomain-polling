6fbe7e8dcddf132f7c07063f113545e83d3e13ed
newfurniturey <newfurniturey@gmail.com>
2013-04-09 21:51:56 -0400

    Added initial timer-intervals for each external library.

--- MemoryAbusingLibrary/MemoryAbusingLibrary.cs
+++ MemoryAbusingLibrary/MemoryAbusingLibrary.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace Remote {
 	public class MemoryAbusingLibrary : IRemoteObject {
+		private const int _blockSize = 1024 * 1024;
+
+		private Timer _timer = null;
 
 		public void Init() {
+			_timer = new Timer(_timerEvent, null, 10000, 10000);
+			Trace.WriteLine("MemoryAbusingLibrary: Init()");
+		}
 
+		public void Dispose() {
+			_timer.Dispose();
+			_timer = null;
 		}
 
 		public void GetMemoryUsage() {
+
+		}
+
+		private static void _timerEvent(object state) {
 
 		}
 
--- NormalLibrary/NormalLibrary.cs
+++ NormalLibrary/NormalLibrary.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Remote {
 	public class NormalLibrary : IRemoteObject {
+		private Timer _timer = null;
 
 		public void Init() {
+			_timer = new Timer(2000);
+			_timer.Elapsed += new ElapsedEventHandler(_timerEvent);
+			_timer.Enabled = true;
+			System.Diagnostics.Trace.WriteLine("NormalLibrary: Init()");
+		}
 
+		public void Dispose() {
+			_timer.Elapsed -= _timerEvent;
+			_timer.Stop();
+			_timer.Dispose();
+			_timer = null;
 		}
 
 		public void GetMemoryUsage() {
 
 		}
 
+		private void _timerEvent(object source, ElapsedEventArgs args) {
+
+		}
 	}
 }
